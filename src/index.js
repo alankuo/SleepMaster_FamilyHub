bd1c021100bc0555419d04728d11bc5126dd7acb
CathlineWL <cathlinewl0113@gmail.com>
2018-11-01 23:02:51 -0700

    change to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// import './index.css';
+// import * as serviceWorker from './serviceWorker';
+// import { BrowserRouter } from 'react-router-dom';
+// import AppRouter from './Router'
+//
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <AppRouter history={BrowserRouter} routes={AppRouter} />
+//   </BrowserRouter>,
+// document.getElementById('root'));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  HashRouter,
+} from 'react-router-dom';
+import App from "./components/App";
 import AppRouter from './Router'
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <AppRouter history={BrowserRouter} routes={AppRouter} />
-  </BrowserRouter>,
-document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+  <HashRouter>
+    <div>
+      <AppRouter routes={AppRouter} />
+    </div>
+  </HashRouter >
+,
+  document.getElementById( 'root' ) );
 serviceWorker.unregister();
