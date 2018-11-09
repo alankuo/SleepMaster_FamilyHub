dba41b23e8dc9f982a97ef785d0485b0ae0bcbfd
Hannah <wangyuhan0214@126.com>
2018-11-08 22:46:32 -0800

    add button on carousel modal

--- src/components/Memories/Modal.js
+++ src/components/Memories/Modal.js
@@ -13,7 +13,9 @@ class Modal extends Component {
     return (
       <div className='popup'>
         <div className='popup_inner'>
-          <img src={photos} className="carousel"></img>          
+          <img src={photos} className="carousel"></img>
+          <button className="glyphicon glyphicon-menu-left button prev"></button>
+          <button className="glyphicon glyphicon-menu-right button next"></button>
           <button onClick={this.props.closeModal}>close</button>
         </div>
       </div>
