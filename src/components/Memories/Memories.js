cff951b0e5902b37ccfbd7a0c3b7fcea6eec9607
Hannah <wangyuhan0214@126.com>
2018-11-08 19:48:58 -0800

    add memory folder

---
+++ src/components/Memories/Memories.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import '../../css/App.css';
+import '../../css/Memories.css';
+import NavBar from '../NavBar';
+import photo from '../../img/bike.jpg';
+import Modal from './Modal.js';
+
+class Memories extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false
+    };
+  }
+
+  showModal() {
+    this.setState({show: true});
+  }
+
+  hideModal() {
+    this.setState({show: false});
+  }
+
+  render() {
+    return (
+      <div classNameName="App">
+        <NavBar />
+        <div className="memory">
+          <div className="year">2018</div>
+          <div className="timeline">
+
+            <p className="time-left">11/07</p>
+            <div className="photos left">
+              <button className="button">
+                <img src={photo} onClick={this.showModal.bind(this)}></img>
+                {this.state.show ?
+                  <Modal closeModal={this.hideModal.bind(this)} />
+                  : null
+                }
+              </button>
+            </div>
+
+            <p className="time-right">11/06</p>
+            <div className="photos right">
+              <button className="button">
+                <img src={photo} onClick={this.showModal.bind(this)}></img>
+                {this.state.show ?
+                  <Modal closeModal={this.hideModal.bind(this)} />
+                  : null
+                }
+              </button>
+            </div>
+
+            <p className="time-left">11/01</p>
+            <div className="photos left">
+              <button className="button">
+                <img src={photo} onClick={this.showModal.bind(this)}></img>
+                {this.state.show ?
+                  <Modal closeModal={this.hideModal.bind(this)} />
+                  : null
+                }
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Memories;
