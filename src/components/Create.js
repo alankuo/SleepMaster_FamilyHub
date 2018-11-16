13e48e9950cc6cfa6e20a14aec26b7072c8b6d8d
Hannah <wangyuhan0214@126.com>
2018-11-15 18:24:29 -0800

    add functions to create, createfamily and joinfamily page

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-
-import NavBar from './NavBar'
+import '../css/Create.css';
+import NavBar from './NavBar';
+import { Link } from 'react-router-dom'
 
 class Create extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
-
+        <div className="create-page">
+          <Link to="/create-family">
+            <button className="create">
+              Create a new family account
+            </button>
+          </Link>
+          <Link to="/join-family">
+            <button className="join">
+              Join an existed family
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
