35be719f1e02c0d58d7eec01d13d39af5749f9a8
Alan Kuo <alk029@ucsd.edu>
2018-11-15 19:08:18 -0800

    add event to localStorage

--- src/components/CreateEvents.js
+++ src/components/CreateEvents.js
@@ -2,39 +2,82 @@ import React, { Component } from 'react';
 import NavBar from './NavBar.js';
 
 import ImageUpload from'./UploadImage';
+import Footer from './Footer'
 
 class CreateEvents extends Component {
   constructor() {
     super();
     // Initial state
     this.state = {
-      routeAddress: "/"
+      routeAddress: "/",
+      imageURL: ""
     };
 
     // Bind all functions so they can refer to "this" correctly
     this.createEvent = this.createEvent.bind(this);
-    this.keyEvent = this.keyEvent.bind (this);
+    this.keyEvent = this.keyEvent.bind(this);
+    this.setImageURL = this.setImageURL.bind(this);
+    this.storeEvent = this.storeEvent.bind(this);
   }
 
   createEvent(e){
-    const event_name = document.getElementById("event_name").value;
-    const number_of_members = document.getElementById("number_of_members").value;
+    const eventName = document.getElementById("event_name").value;
+    const numOfMembers = document.getElementById("number_of_members").value;
+    const equipment = document.getElementById("equipment").value;
+    const time = document.getElementById("time").value;
+    const categorySelecter = document.getElementById("categorySelector");
+    const categoryId = categorySelecter.options[categorySelecter.selectedIndex].value;
+    const categoryText = categorySelecter.options[categorySelecter.selectedIndex].text;
+    const description = document.getElementById("description").value;
 
-    if(event_name===""){
+    if(eventName === ""){
       alert("Please fill in the event name!");
     }
-    else if(number_of_members===""){
-      alert("Please fill in the event name!");
+    else if(numOfMembers === ""){
+      alert("Please fill in the number of members suggested!");
+    }
+    else if(categoryId === "0") {
+      alert("Please select a category for the event!");
+    }
+    else if(this.state.imageURL === "") {
+      alert("Please select an image for the event!");
     }
     else{
       alert ("The event has been successfully created!");
+      this.storeEvent(eventName, numOfMembers, categoryText, equipment, time, description);
+      window.location = "#/";
     }
   }
 
-  keyEvent (e) {
+  keyEvent(e) {
     if (e.key === 'Enter') {
       this.createEvent();
     }
+  }
+
+  setImageURL(url) {
+    this.setState({imageURL: url});
+  }
+
+  storeEvent(eventName, numOfMembers, categoryText, equipment, time, description) {
+    let activityArray = localStorage["activities"];
+    if(activityArray === undefined) {
+      activityArray = [];
+    }
+    else {
+      activityArray = JSON.parse(activityArray);
+    }
+    let eventObj = {
+      "name": eventName,
+      "num": numOfMembers,
+      "category":categoryText,
+      "equipment":equipment,
+      "time-length":time,
+      "img":this.state.imageURL,
+      "description":description
+    };
+    activityArray.push(eventObj);
+    localStorage.setItem('activities', JSON.stringify(activityArray));
   }
 
   render() {
@@ -42,45 +85,50 @@ class CreateEvents extends Component {
     return (
       <div>
         <NavBar/>
-        <div className="container" onKeyPress={this.keyEvent}>
+        <div className="container">
           <div className="settings-bar">
             <div className="header">
               <h2>Create an Event</h2>
             </div>
             <form method="post">
-              <div className="input-group">
+              <div className="input-group" onKeyPress={this.keyEvent}>
                 <label>Event Name*</label>
                 <input type="text" name="event_name" id="event_name" placeholder=""></input>
               </div>
-              <div className="input-group">
+              <div className="input-group" onKeyPress={this.keyEvent}>
                 <label>Number of members*</label>
                 <input type="number" name="number_of_members" id="number_of_members" placeholder=""></input>
               </div>
 
-              <div class="input-group">
+              <div className="input-group" onKeyPress={this.keyEvent}>
                 <label>Category*</label>
 
-                <select class="custom-select" id="inputGroupSelect01">
-                  <option selected>Choose Category</option>
+                <select className="custom-select" id="categorySelector">
+                  <option value="0" defaultValue>Choose Category</option>
                   <option value="1">Outdoor</option>
                   <option value="2">Indoor</option>
                   <option value="3">Others</option>
                 </select>
               </div>
 
-              <div className="input-group">
-                <label>Equipment Needed</label>
-                <input type="text" name="need" id="need" placeholder=""></input>
-              </div>
-
-              <div className="input-group">
+              <div className="input-group" onKeyPress={this.keyEvent}>
                 <label>Approximated Time (in minutes)</label>
                 <input type="number" name="time" id="time" placeholder=""></input>
               </div>
 
+              <div className="input-group" onKeyPress={this.keyEvent}>
+                <label>Equipment Needed</label>
+                <input type="text" name="equipment" id="equipment" placeholder=""></input>
+              </div>
+
               <div className="input-group">
+                <label>Description</label>
+                <textarea style={{height:100+'px',width:400+'px'}} type="text" name="description" id="description" placeholder="Please enter the description for the event..."></textarea>
+              </div>
+
+              <div className="input-group" onKeyPress={this.keyEvent}>
                 <label>Event Photo*</label>
-                <ImageUpload/>
+                <ImageUpload setImageURL={this.setImageURL}/>
               </div>
 
               <div>
@@ -89,6 +137,7 @@ class CreateEvents extends Component {
             </form>
           </div>
         </div>
+        <Footer />
       </div>
     );
   }
